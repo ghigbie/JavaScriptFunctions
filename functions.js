88ded571308ca1f2891f59553097b062e292fdbb
George Higbie <gehigbie@gmail.com>
2017-01-13 13:36:53 -0700

    added one function

--- functions.js
+++ functions.js
@@ -1,1 +1,11 @@
 //this file was created in the command line
+
+function sayHi(){
+	console.log("Hello!");
+	console.log("Goodbye!");
+}
+
+sayHi();
+
+sayHi //this will only refer to the function 
+
